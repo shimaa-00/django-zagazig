08c65f403551252305c8f2b11af05643d5c594bd
Shimaa Khallaf <shimaakhallaf@Shimaas-MacBook-Pro.local>
2025-10-29 12:32:47 +0300

    day2

--- student/models.py
+++ student/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Student (models.Model):
+    name = models.CharField(max_length=50) # varchar 50
+    age = models.IntegerField()
+    gpa = models.DecimalField(max_digits=3 , decimal_places=2)
+    gender = models.CharField(max_length=10,choices= [("m" , "male") , ("f" , "female")])
+    
+    def __str__(self):
+        return self.name
--- student/urls.py
+++ student/urls.py
@@ -2,8 +2,8 @@ from django.urls import path , include
 from .views import handle_student_request , student_name, student_id , return_index_html
 urlpatterns = [
     # 127.0.0.1:8000/student
-    path('' , handle_student_request),
-    path('index_html' , return_index_html ),
-    path ("<int:id>" , student_id) , # variable sent to function 
-    path ("<str:name>" , student_name), # variable sent to function    
+    path('' , handle_student_request , name = 'students' ),
+    path('index_html' , return_index_html  , name = 'studentindex'),
+    path ("<int:id>" , student_id , name = 'student_id') , # variable sent to function 
+    path ("<str:name>" , student_name  , name = 'student_name' ), # variable sent to function    
 ]
